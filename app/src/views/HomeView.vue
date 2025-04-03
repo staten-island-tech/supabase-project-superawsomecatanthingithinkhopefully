9c04f5734f5160549ec70d24b782aff7e0c61d4a
EyadAbdelfatah <144480862+EyadAbdelfatah@users.noreply.github.com>
2025-04-03 13:58:58 -0400

    jgk

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <button class="btn">Default</button>
+    <button class="btn btn-secondary">Secondary</button>
+  </main>
+</template>
