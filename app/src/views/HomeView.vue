573fa72ec64702a0e7eb9a2e26f8c9df677d7a32
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2025-04-07 14:00:33 -0400

    set up

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,13 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <button class="btn">Default</button>
-    <button class="btn btn-secondary">Secondary</button>
-  </main>
+  <div></div>
 </template>
+
+<script setup></script>
+
+<style scoped></style>
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -7,10 +7,14 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss(), vue()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 })
