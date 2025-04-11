ad121897529b80c12e2773ab83df4ad1aaba2aff
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2025-04-11 13:56:52 -0400

    log in page tailwind

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
-    <h1 class="text-blue-600">kjhgadsfjhy</h1>
-    <button class="btn btn-secondary">laksjdhfgf</button>
+    <h1 class="text-blue-600">Welcome to CONQUEST</h1>
+
+    <form action="" class="form" @submit.prevent="submit()">
+      <label for="username">Username:</label>
+      <input type="text" class="username" placeholder="Input Username" />
+      <label for="password">Password:</label>
+      <input type="text" class="password" placeholder="Input Password" />
+      <div>
+        <button type="submit" class="btn">Log In</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script setup>
-//import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router'
+import { reactive } from 'vue'
+function submit() {
+  console.log('Mathias Papias')
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,5 +1,14 @@
 <template>
   <div></div>
+  <form action="" class="form" @submit.prevent="submit()">
+    <label for="username">Username:</label>
+    <input type="text" class="username" placeholder="Input Username" />
+    <label for="password">Password:</label>
+    <input type="text" class="password" placeholder="Input Password" />
+    <div>
+      <button type="submit" class="btn">Log In</button>
+    </div>
+  </form>
 </template>
 
 <script setup></script>
