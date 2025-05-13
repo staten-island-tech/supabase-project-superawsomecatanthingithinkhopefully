033a0fefd51bf982cff2b1c281aacc99ddfd75f6
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2025-05-13 14:00:07 -0400

    theme being poopy

--- app/src/components/LogIn.vue
+++ app/src/components/LogIn.vue
@@ -1,57 +1,59 @@
 <template>
-  <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-      <img class="mx-auto h-10 w-auto" src="/Logo.png" alt="Your Company" />
-      <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">log</h2>
-    </div>
+  <div>
+    <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+      <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+        <img class="mx-auto h-50 w-auto" src="/Logo.png" alt="Your Company" />
+        <h2 class="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">log</h2>
+      </div>
 
-    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-      <form @submit.prevent="handleLog" class="space-y-6" action="#" method="POST">
-        <div>
-          <label for="email" class="block text-sm/6 font-medium text-white">Email address</label>
-          <div class="mt-2">
-            <input
-              v-model="signupStore.email"
-              type="email"
-              name="email"
-              id="email"
-              autocomplete="email"
-              required
-              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-            />
+      <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form @submit.prevent="handleLog" class="space-y-6" action="#" method="POST">
+          <div>
+            <label for="email" class="block text-sm/6 font-medium text-white">Email address</label>
+            <div class="mt-2">
+              <input
+                v-model="signupStore.email"
+                type="email"
+                name="email"
+                id="email"
+                autocomplete="email"
+                required
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
+            </div>
           </div>
-        </div>
 
-        <div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="block text-sm/6 font-medium text-white">Password</label>
+          <div>
+            <div class="flex items-center justify-between">
+              <label for="password" class="block text-sm/6 font-medium text-white">Password</label>
+            </div>
+            <div class="mt-2">
+              <input
+                v-model="signupStore.password"
+                type="password"
+                name="password"
+                id="password"
+                autocomplete="current-password"
+                required
+                class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+              />
+            </div>
           </div>
-          <div class="mt-2">
-            <input
-              v-model="signupStore.password"
-              type="password"
-              name="password"
-              id="password"
-              autocomplete="current-password"
-              required
-              class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-            />
-          </div>
-        </div>
 
-        <div>
-          <button
-            type="submit"
-            class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            log unc
-          </button>
-        </div>
-        <div v-if="signupStore.error" class="text-red-500">
-          {{ signupStore.error }}
-        </div>
-      </form>
-      <RouterLink to="/signup">Don't have an account Dign up?</RouterLink>
+          <div>
+            <button
+              type="submit"
+              class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              log unc
+            </button>
+          </div>
+          <div v-if="signupStore.error" class="text-red-500">
+            {{ signupStore.error }}
+          </div>
+        </form>
+        <RouterLink to="/signup">Don't have an account Dign up?</RouterLink>
+      </div>
     </div>
   </div>
 </template>
@@ -61,6 +63,8 @@ import { useSignupStore } from '@/stores/authuser'
 import { useRouter } from 'vue-router'
 import { supabase } from '@/lib/supabaseClient'
 import { RouterLink } from 'vue-router'
+import { onMounted } from 'vue'
+
 const router = useRouter()
 const signupStore = useSignupStore()
 async function handleLog() {
