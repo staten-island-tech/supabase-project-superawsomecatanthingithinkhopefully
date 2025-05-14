9393058ac69ef4d30aaa10b2911d933df65676e8
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2025-05-14 14:00:50 -0400

    GSAP is a little special

--- app/src/components/MainDashboard.vue
+++ app/src/components/MainDashboard.vue
@@ -1,40 +1,59 @@
 <template>
+  <div data-theme="synthwave" class="min-h-screen">
+    <div class="box gradient-green green"></div>
+    <div class="box gradient-purple purple"></div>
+    <div class="box gradient-blue blue"></div>
+
     <div>
       <button>Join a game</button>
-      
+
       <form @submit.prevent="handleRoom">
-    <input 
-      type="number" 
-      v-model.number="use_rooms.number_player" 
-      required 
-      min="1" 
-      placeholder="Number of players" 
-    />
-    <button type="submit">Create Room</button>
-  </form>
+        <input
+          type="number"
+          v-model.number="use_rooms.number_player"
+          required
+          min="1"
+          placeholder="Number of players"
+        />
+        <button type="submit">Create Room</button>
+      </form>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { gsap } from 'gsap'
 import { useSignupStore } from '@/stores/authuser'
 import { useRouter } from 'vue-router'
 import { supabase } from '@/lib/supabaseClient'
 import { rooms } from '@/stores/rooms'
 import routers from '@/router'
+import { onMounted } from 'vue'
+
 const use_rooms = rooms()
 const router = useRouter()
-async function handleRoom(){
+async function handleRoom() {
   const result = await use_rooms.makeRoom()
   console.log(use_rooms.number_player)
-  if(result){
-    router.push({path:`/${result['id']}`})
+  if (result) {
+    router.push({ path: `/${result['id']}` })
   }
-  
 }
+onMounted(() => {
+  console.log(1)
+  gsap.to('.green', { rotation: 360, x: 100, duration: 1 })
 
+  // target the element with a class of "purple" - rotate and move FROM 100px to the left over the course of 1 second.
+  gsap.from('.purple', { rotation: -360, x: -100, duration: 1 })
 
+  // target the element with a class of "blue" - rotate and move FROM 100px to the left, TO 100px to the right over the course of 1 second.
+  gsap.fromTo('.blue', { x: -100 }, { rotation: 360, x: 100, duration: 1 })
+})
 </script>
 
 <style scoped>
-
+.box {
+  width: 500px;
+  height: 500px;
+}
 </style>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -7,6 +7,7 @@
 <script setup lang="ts">
 import LogIn from '@/components/LogIn.vue'
 import { onMounted } from 'vue'
+import { gsap } from 'gsap'
 </script>
 
 <style scoped></style>
