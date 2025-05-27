a9b4bfd131d4873381a36f1923aef776b6c73979
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2025-05-27 13:27:40 -0400

    Update AccountStuff.vue

--- app/src/views/AccountStuff.vue
+++ app/src/views/AccountStuff.vue
@@ -7,17 +7,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { supabase } from '@/lib/supabaseClient'
-
-test()
-async function test(){
-  const {data:{user}, error:authError} = await supabase.auth.getUser()
-  console.log(user)
-  const { data:{username}, error } = await supabase.from('profiles').select('username')
-  console.log(username)
-  //username null right now
-  
-}
-
 </script>
 
 <style scoped></style>
