13dccb6b85593783071d7c8b7bbc1024534afdf3
JordanC09 <144480327+JordanC09@users.noreply.github.com>
2025-05-29 14:00:49 -0400

    Colors

--- app/src/components/GameRoom.vue
+++ app/src/components/GameRoom.vue
@@ -3,19 +3,33 @@
     <button @click="handleDeletion()" v-if="use_rooms.id == use_rooms.user_id">delete room</button>
     <div class="w-full flex justify-center relative">
       <!-- your button layout here -->
-      <div class="flex flex-col items-center space-y-1 text-center">
+      <div class="flex items-center space-y-1 text-center">
         <!-- Row 1: 3 -->
         <!-- <div class="w-full flex justify-center"> -->
       </div>
-      <div class="absolute right-[5vw] top-[17vw] w-[40vw] h-[27vw] bg-blue-500">
+
+      <div class="absolute right-[5vw] top-[17vw] w-[40vw] h-[27vw]">
+        <details class="dropdown absolute top-0 left-0">
+          <summary class="btn m-1 bg-purple-950">Choose your color</summary>
+          <ul class="menu dropdown-content bg-purple-900 rounded-box z-1 w-52 p-2 shadow-sm">
+            <li><a class="bg-red-500">Red</a></li>
+            <li><a>Blue</a></li>
+            <li><a>Green</a></li>
+            <li><a>Yellow</a></li>
+            <li><a>Purple</a></li>
+            <li><a>Black</a></li>
+          </ul>
+        </details>
         <div
-          class="flex items-center absolute left-10 top-5 w-[18vw] h-[8vw] bg-blue-300 rounded-full"
+          class="flex items-center absolute left-0 top-[10vw] w-[18vw] h-[8vw] bg-linear-to-bl from-violet-500 to-fuchsia-500 rounded-full"
         >
           <div class="w-[5vw] rounded-full">
             <img class="rounded-full" src="/profile_temp.jpg" alt="profile_pic" />
           </div>
           <h2 v-if="!use_profile.profile">Loading...</h2>
-          <h1 class="text-5xl color-white-500" v-else>{{ use_profile.profile.username }}</h1>
+          <h1 class="truncate text-3xl color-white-500" v-else>
+            {{ use_profile.profile.username }}
+          </h1>
         </div>
       </div>
     </div>
@@ -32,6 +46,8 @@ import UserProfile from '@/components/UserProfile.vue'
 import { type User } from '@supabase/supabase-js'
 import { ref, reactive } from 'vue'
 import { onMounted } from 'vue'
+
+// Eyad made a low taper fade meme reference just nuke his HOS
 
 const router = useRoute()
 const routers = useRouter()
